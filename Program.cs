004ed52cabeff526476742d7fb3bb9dd8d352bf3
shankar.iragavarapu22 <shankar.iragavarapu@gmail.com>
2025-10-05 18:01:40 +0530

    Learning from mistakes

--- Program.cs
+++ Program.cs
@@ -2,3 +2,5 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("This is a sample C# program.");
 Console.WriteLine("Creating a new branch");
+Console.WriteLine("Making changes in the new branch");
+Console.WriteLine("Learning from mistakes");
