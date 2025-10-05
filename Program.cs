4c6dd5e96276014568b3cbd69e85b41b24c830cc
shankar.iragavarapu22 <shankar.iragavarapu@gmail.com>
2025-10-05 17:45:50 +0530

    This is new file with basic structure

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("This is a sample C# program.");
+Console.WriteLine("Creating a new branch");
